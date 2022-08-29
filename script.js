c2c2bbd454a234a37cf164d0505cb0d143f0d898
jay-computer <joshuakang19@gmail.com>
2022-08-29 13:35:51 -0400

    Initialize symbol insertion via players

---
+++ script.js
@@ -0,0 +1,27 @@
+let xTurn = true;
+
+function winCondition() {
+    
+}
+
+function addSymbol(e) {
+    if(xTurn) {
+        e.target.innerHTML = "X";
+        console.log(e);
+        xTurn = false;
+
+        e.target.removeEventListener('click', addSymbol);
+
+        winCondition();
+    }
+
+    else {
+        e.target.innerHTML = "O";
+        xTurn = true;
+
+        winCondition();
+    }
+}
+
+const squares = document.querySelectorAll(".game-square");
+squares.forEach(square => square.addEventListener('click', addSymbol));
